567638cba2061f04e2d9138e2c531d917f923152
Ganesh Reddy <edulaganeshreddy123@gmail.com>
2023-04-17 19:43:22 +0530

    changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :print_display_message
+
+  after_action :print_display
+
+  private
+
+  def print_display_message
+    puts '-------------------Iam inherited before action filter----------------------------'
+  end
+
+  def print_display
+    puts '================================================================================'
+    puts '-------------------Iam inherited after action filter----------------------------'
+  end
 end
